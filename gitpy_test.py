a3c7efd85dcfedc5e4342f84351b556f31c416f9
WF <wf@WFs-MacBook-Pro.local>
2014-02-12 11:34:19 -0500

    some more py git stuff

---
+++ gitpy_test.py
@@ -0,0 +1,88 @@
+
+class Board(object):
+    def __init__(self):
+        self.rows = [[0] * 3 for _ in range(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.    
+;; If you want to create a file, visit that file with C-x C-f,                  
+;; then enter the text in that file's own buffer.
+
+class Board(object):
+    def __init__(self):
+        self.rows = [[0] * 3 for _ in range(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-uuu:**-F1  *scratch*      All L7     (Python)----------------------------------
+
