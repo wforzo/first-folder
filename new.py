00325ebf22c637828a0e7c3426dd7d229f373683
WF <wf@WFs-MacBook-Pro.local>
2014-02-12 11:37:05 -0500

    another messages

--- new.py
+++ new.py
@@ -1,3 +1,8 @@
 class Board(object):
     def __init__(self):
         self.rows = [[0] * 3 for _ in range(3)
+
+    @property
+    def columns(self):
+                return zip(*self.rows)
+    @property
